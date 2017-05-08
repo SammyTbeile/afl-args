c5bc7d5f98c626ba2551bdc487d0eb3104bab27f
Joel Cretan <jcretan@gmail.com>
2017-05-07 22:52:27 -0400

    -Q should not have : ... it doesn't take another argument

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -7708,7 +7708,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q:a")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Qa")) > 0)
 
     switch (opt) {
 
